5080128f7fec6b8b354f343fc285e1c077fa8f84
toprakkoc <hello@toprak.dev>
2021-02-25 21:57:44 +0300

    added auto save

--- src/Containers/WeatherList/index.js
+++ src/Containers/WeatherList/index.js
@@ -1,10 +1,12 @@
-import { useState } from 'react';
+import { useState, useContext, useEffect } from 'react';
+import { StorageContext } from '../../Providers/StorageProvider';
 import WeatherBlock from '../../Components/WeatherBlock';
 import SearchBlock from '../../Components/SearchBlock';
 import './index.css';
 
 const WeatherList = () => {
-  const [locations, setLocations] = useState(['Ankara']);
+  const [locations, setLocations] = useState([]);
+  const { saveLocationsToStorage, getLocationsFromStorage } = useContext(StorageContext); 
 
   const createWeatherBlocks = () => {
     return locations.map((location, key) => {
@@ -12,9 +14,19 @@ const WeatherList = () => {
     });
   };
 
+  useEffect(() => {
+    const locationsFromStorage = getLocationsFromStorage();
+
+    setLocations(locationsFromStorage.length ? locationsFromStorage : []);
+  }, [getLocationsFromStorage])
+
+  useEffect(() => {
+    saveLocationsToStorage(locations);
+  }, [saveLocationsToStorage, locations]);
+
   return (
     <div className="weather-list">
-      {locations.length && createWeatherBlocks()}
+      {locations.length ? createWeatherBlocks() : ''}
       <SearchBlock locations={locations} updateLocations={setLocations}/>
     </div>
   );
